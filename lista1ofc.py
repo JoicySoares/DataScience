733b4fa85c807e41b212789c4f0d46169c61e8a2
JoicySoares <soaresjoicy1@gmail.com>
2021-03-26 19:54:52 -0300

    first commit

---
+++ lista1ofc.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+
+V1 = [1, 3, 5, 7, 8, 9, 20, 21, 22, 25, 30, 35]
+
+V2 = [-3, -4, 2, 5, 12, 8]
+
+sns.displot(V1)
